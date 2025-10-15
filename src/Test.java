f532938dc490ff6fd392aa961487882ee04908c0
gavendra123-bit <pachahragavendra@gmail.com>
2025-10-15 14:56:18 +0530

    Testing_New_Thread_Edited

--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,19 @@
-//public class World extends Thread {
-//    @Override
-//    public void run() {
-//        for(; ; ){
-//            System.out.println("world");
-//        }
-//    }
-//}
+class World extends Thread {
+    @Override
+    public void run() {
+        for(; ; ){
+            System.out.println("world");
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
 public class Test {
     public static void main(String[] args){
         World world = new World();
         world.start();
         for(int i=1;i<11 ;i++ ){
             System.out.println("Hello");
-        }
-    }
-}
-class World extends Thread {
-    @Override
-    public void run() {
-        for(int i=1;i<11 ;i++ ){
-            System.out.println("world");
+            System.out.println(Thread.currentThread().getName());
         }
     }
 }
